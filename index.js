d39569bb62f62b4da4cd2b7f4f4898fa489970c2
Unknown <fangkyi02@live.cn>
2017-07-24 15:29:37 +0800

    提交
    提交

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ import { WebView, View, StyleSheet } from 'react-native';
 
 const BODY_TAG_PATTERN = /\<\/ *body\>/;
 
-// Do not add any comments to this! It will break because all line breaks will removed for
-// some weird reason when this script is injected.
 var script = `
 ;(function() {
 var wrapper = document.createElement("div");
@@ -48,20 +46,18 @@ ${script}
 
 const codeInject = (html) => html.replace(BODY_TAG_PATTERN, style + "</body>");
 
-/* @flow */
-
 export default class AutoHeightWebView extends Component {
+
   render() {
     const {style,webStyle,webUrl} = this.props
     return (
-      <View style={[styles.container,{style}]}>
-        <WebView
+      <View style={[styles.container,style]}>
           <WebView
-           scrollEnabled = {false}
+           ref={webview => thisWebView = webview}
            style={[styles.webView,webStyle]}
-           source={{uri:codeInject(webUrl)}}
+           scrollEnabled = {false}
+           source={{html:'<p style="margin-top: 20px; margin-bottom: 20px; padding: 0px; font-size: 14px; color: rgb(68, 68, 68); font-family: &quot;Microsoft YaHei&quot;, Tahoma, Arial, 宋体; line-height: 24px; white-space: normal; widows: 1; background-color: rgb(255, 255, 255);">（1）在房间里放上几盒开盖的风油精、清凉油，或在墙上涂点薄荷；在身上或枕头上洒些香水；将樟脑丸磨碎、撒在屋内墙角；在室内的花盆里栽一两株西红柿，西红柿枝叶发出的气味会把蚊子赶走。</p><p style="margin-top: 20px; margin-bottom: 20px; padding: 0px; font-size: 14px; color: rgb(68, 68, 68); font-family: &quot;Microsoft YaHei&quot;, Tahoma, Arial, 宋体; line-height: 24px; white-space: normal; widows: 1; background-color: rgb(255, 255, 255);">（2）在灯下挂一把香葱，或用纱袋装几根葱段，各种小虫都不会飞来；用橘红色玻璃纸或绸布套在灯泡上，蚊子最怕橘红色光。</p><p><br/></p>'}}
          />
-        />
       </View>
     );
   }
@@ -69,7 +65,7 @@ export default class AutoHeightWebView extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    height:'100%'
   },
   webView:{
     height:'100%'
